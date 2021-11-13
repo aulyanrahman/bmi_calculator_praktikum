6f384221ccec0772f7cc3d9e6fa550bd03637236
aulyanrahman <aulyaasli@gmail.com>
2021-11-13 12:26:29 +0800

    adding widget

---
+++ lib/input_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({ Key? key }) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          // Widget Jenis Kelamin
+          Expanded(child: Row(
+            children: [
+              Expanded(child: child),
+              Expanded(child: child)
+            ],
+          )),
+          // Widget Tinggi
+          Expanded(child: Row(children: [
+            Expanded(child: child)
+          ],)),
+          // Widget Berat dan Umur
+          Expanded(child: Row(children: [
+            Expanded(child: child),
+            Expanded(child: child),
+          ],)),
+          // Widget Tombol Hitung
+          Expanded(child: Row(children: [
+            Expanded(child: child)
+          ],))
+        ],
+      )
+    );
+  }
+}
+
+Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        height: 200.0,
+        width: 179.0,
+      ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -12,32 +13,6 @@ class BMICalculator extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E22),
-      ),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  const InputPage({ Key? key }) : super(key: key);
-
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
       ),
     );
   }
