bbe93ecebf65061f0e0c4798d4bec6930d30af51
aulyanrahman <aulyaasli@gmail.com>
2021-11-13 12:52:45 +0800

    using reusable card

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,73 +20,56 @@ class _InputPageState extends State<InputPage> {
           // Widget Jenis Kelamin
           Expanded(child: Row(
             children: [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                height: 200.0,
-                width: 179.0,
-              ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                height: 200.0,
-                width: 179.0,
-              ),)
+              Expanded(
+                child: ReusableCard(),
+              ),
+              Expanded(
+                child: ReusableCard(),
+              ),
             ],
           )),
           // Widget Tinggi
           Expanded(child: Row(children: [
-            Expanded(child: Container(
-              margin: EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              height: 200.0,
-              width: 179.0,
-            ),)
+            Expanded(
+              child: ReusableCard(),
+            ),
           ],)),
           // Widget Berat dan Umur
           Expanded(child: Row(children: [
-            Expanded(child: Container(
-              margin: EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              height: 200.0,
-              width: 179.0,
-            ),),
-            Expanded(child: Container(
-              margin: EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              height: 200.0,
-              width: 179.0,
-            ),),
+            Expanded(
+              child: ReusableCard(),
+            ),
+            Expanded(
+              child: ReusableCard(),
+            ),
           ],)),
           // Widget Tombol Hitung
           Expanded(child: Row(children: [
-            Expanded(child: Container(
-              margin: EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              height: 200.0,
-              width: 179.0,
-            ),)
+            Expanded(
+              child: ReusableCard(),
+            ),
           ],))
         ],
       )
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      height: 200.0,
+      width: 179.0,
     );
   }
 }
