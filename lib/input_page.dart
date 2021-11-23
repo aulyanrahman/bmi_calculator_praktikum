a190fb325135e2c45fd3dfa921738e7ac970ad01
aulyanrahman <aulyaasli@gmail.com>
2021-11-23 12:04:06 +0800

    separating class to different file

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour      = Color(0xFF1D1E33);
@@ -27,17 +29,25 @@ class _InputPageState extends State<InputPage> {
             children: [
               Expanded(
                 child: ReusableCard(
-                colour: activeCardColour,
-                height: 200.0,
-                width: 179.0,
-              ),
+                  colour: activeCardColour,
+                  height: 200.0,
+                  width: 179.0,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
+                ),
               ),
               Expanded(
                 child: ReusableCard(
-                colour: activeCardColour,
-                height: 200.0,
-                width: 179.0,
-              ),
+                  colour: activeCardColour,
+                  height: 200.0,
+                  width: 179.0,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus, 
+                    label: 'FEMALE',
+                  ),
+                ),
               ),
             ],
           )),
@@ -77,28 +87,6 @@ class _InputPageState extends State<InputPage> {
           )
         ],
       )
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, required this.height, required this.width, this.cardChild});
-  
-  final Color colour;
-  final Widget? cardChild;
-  double height, width;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      height: height,
-      width: width,
     );
   }
 }
---
+++ lib/reusable_card.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour, required this.height, required this.width, this.cardChild});
+  
+  final Color colour;
+  final Widget? cardChild;
+  double height, width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      height: height,
+      width: width,
+    );
+  }
+}
