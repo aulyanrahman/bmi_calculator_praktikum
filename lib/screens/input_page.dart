f7360840a3ec559611b35ca6ce850ac972fd63d0
aulyanrahman <aulyaasli@gmail.com>
2021-11-24 11:20:04 +0800

    separating file to different folder, create constant file

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'reusable_card.dart';
+import '../components/icon_content.dart';
+import '../components/reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour      = Color(0xFF1D1E33);
